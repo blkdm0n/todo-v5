26e2dd4a5fa7a205df556818a5a64d52ecd96f6a
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-07-17 18:01:13 -0700

    Requirement 1 complete, displayTodos shows the todoText property

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # todo-v5
 Version 5: Loops of Logic
+
+[X] displayTodos should show .todoText
+[] displayTodos should tell you if .todos is empty
+[] displayTodos should show completed
+
---
+++ index.js
@@ -0,0 +1,31 @@
+var todoList = {
+  todos: [],
+  displayTodos: function () {
+    for (let i = 0; i < this.todos.length; i++) {
+      console.log(this.todos[i].todoText);
+    }
+  },
+  addTodo: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false,
+    });
+    this.displayTodos();
+  },
+  changeTodo: function (oldTodo, newTodo) {
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].todoText === oldTodo) {
+        this.todos[i].todoText = newTodo;
+      }
+    }
+    this.displayTodos();
+  },
+  deleteTodo: function (deletedTodo) {
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].todoText === deletedTodo) {
+        this.todos.splice(i, 1);
+      }
+    }
+    this.displayTodos();
+  }
+}
